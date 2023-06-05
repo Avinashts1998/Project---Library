1b81c9c2e67a7bff57b91e2fe1747f2c5427f68d
= <=>
2023-06-05 14:21:34 +0530

    get all books

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
+const router = require('./src/routers/libraryRouter')
+
+
 const db = require('./config/dbConnection')
 
-
 const app = express()
-dotenv.config({path: "./config.env"})
+dotenv.config({ path: "./config.env" }) // env file configured
+
+global.clientConnection = db.initClientDbConnection() // Db connection enabled by calling
+
+app.set('table', path.join(__dirname, 'table'));
+app.set("view engine", "ejs");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/', router)
 
 
-app.get("/", (request, response)=>{
-    response.send('Hello World!')
-   
-})
-
-global.clientConnection = db.initClientDbConnection()
-
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`App listening to the port :` + " " + process.env.PORT + " " + "Enne Kollathirikkan Pattumo!!!")
 })
