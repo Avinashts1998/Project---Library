31b5e862d9bf97eb4e03ec1dfc4c76f66899fe3d
= <=>
2023-06-05 14:21:49 +0530

    get all books

--- src/controllers/libraryCotroller.js
+++ src/controllers/libraryCotroller.js
@@ -1,1 +1,20 @@
-// controller file //
+
+const LibraryFunction = require('./../utils/libraryFunction')
+
+
+exports.getAllBooks = async (req, res, next) => {
+
+    LibraryFns = new LibraryFunction()
+
+    const argsData = (req.body.args) ? req.body.args : "";
+    const authData = (argsData.auth) ? argsData.auth : "No data";
+
+    const data = await LibraryFns.getAllBooks(argsData, authData)
+    // console.log(authData)
+
+    res.status(200).json({
+        data: data,
+        success: true,
+        result: false
+    })
+}
