fe835a07c75c3ed6c8c1200ea233d569d0200c90
= <=>
2023-06-05 14:22:29 +0530

    get all books

--- src/routers/libraryRouter.js
+++ src/routers/libraryRouter.js
@@ -1,1 +1,12 @@
-// router file ///
+const express = require('express')
+const libraryController = require('./../controllers/libraryCotroller')
+
+
+const router = express.Router()
+
+router
+    .route("/get-all-books")
+    .post(libraryController.getAllBooks)
+
+
+module.exports = router // export 
