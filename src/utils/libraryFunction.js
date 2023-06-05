b201e28a5ba67a5a994b0ddc56c1f1d6626c5d03
= <=>
2023-06-05 14:22:40 +0530

    get all books

--- src/utils/libraryFunction.js
+++ src/utils/libraryFunction.js
@@ -1,1 +1,20 @@
-// function file //
+const staticFunction = require('./../utils/staticFunction')
+
+class LibraryFunction {
+
+    getAllBooks = async (argsData, authData) => {
+
+
+        const data = staticFunction.getAllBooks(argsData, authData)
+
+
+
+        return data;
+
+
+    }
+
+
+}
+
+module.exports = LibraryFunction;
