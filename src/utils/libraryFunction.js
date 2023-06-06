15f3b0639a1723e40738cd84b9181756b35e2fca
= <=>
2023-06-06 15:31:58 +0530

    insert and get books

--- src/utils/libraryFunction.js
+++ src/utils/libraryFunction.js
@@ -1,17 +1,44 @@
 const staticFunction = require('./../utils/staticFunction')
+const Books_model = require("./../models/booksModel")
 
 class LibraryFunction {
 
-    getAllBooks = async (argsData, authData) => {
+    getAllBooks = async (dbName, booksColection) => {
+        try {
+            const dbConnection = await global.clientConnection
+            const librrayDB = await dbConnection.useDb(dbName)
+
+            const booksModel = librrayDB.model(booksColection, Books_model.BookSchema, booksColection)
+            let res = ""
+            const result = await booksModel.find({}).then((data) => {
+                res = data
+            })
+
+            console.log(res)
+
+            return res;
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
-        const data = staticFunction.getAllBooks(argsData, authData)
+    insertBooks = async (dbName, booksColection, paramsData) => {
+        try {
+            const dbConnection = await global.clientConnection
+            const librrayDB = await dbConnection.useDb(dbName)
 
+            const booksModel = librrayDB.model(booksColection, Books_model.BookSchema, booksColection)
 
+            const result = await booksModel.create(paramsData).then(
+                console.log("Data Inserted Succesfuly!")
+            )
 
-        return data;
-
-
+            return paramsData;
+        } catch (error) {
+            console.log(error)
+        }
     }
 
 
