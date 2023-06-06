47fcd76ab446343ed6740b6654c3e2cde29e8b5e
= <=>
2023-06-06 15:33:09 +0530

    insert and get books

---
+++ src/utils/staticFunction.js
@@ -0,0 +1,60 @@
+
+exports.getAllBooks = async (argsData, authData) => {
+    const books = [
+        {
+            "Book No": "10875",
+            "Name": "Randamoozham",
+            "Auther": "MT Vasudevan Nair",
+            "Publication": "Current Books",
+            "Category": "Novel",
+            "Shelf No": 8,
+            "Row No": 2,
+            "Published Date": "11-12-1987",
+            "Description": "The novel is a retelling of the Indian epic Mahabharata from the perspective of Bhima, the second Pandava. The story deviates from the traditional Mahabharata story as it avoids the divine elements of the ancient epic and re-represent the characters and events realistically.",
+            "purchased year": "2000",
+            "image": "image.jpg",
+        },
+        {
+            "Book No": "10875",
+            "Name": "Randamoozham",
+            "Auther": "MT Vasudevan Nair",
+            "Publication": "Current Books",
+            "Category": "Novel",
+            "Shelf No": 8,
+            "Row No": 2,
+            "Published Date": "11-12-1987",
+            "Description": "The novel is a retelling of the Indian epic Mahabharata from the perspective of Bhima, the second Pandava. The story deviates from the traditional Mahabharata story as it avoids the divine elements of the ancient epic and re-represent the characters and events realistically.",
+            "purchased year": "2000",
+            "image": "image.jpg",
+        },
+        {
+            "Book No": "10875",
+            "Name": "Randamoozham",
+            "Auther": "MT Vasudevan Nair",
+            "Publication": "Current Books",
+            "Category": "Novel",
+            "Shelf No": 8,
+            "Row No": 2,
+            "Published Date": "11-12-1987",
+            "Description": "The novel is a retelling of the Indian epic Mahabharata from the perspective of Bhima, the second Pandava. The story deviates from the traditional Mahabharata story as it avoids the divine elements of the ancient epic and re-represent the characters and events realistically.",
+            "purchased year": "2000",
+            "image": "image.jpg",
+        },
+        {
+            "Book No": "10875",
+            "Name": "Randamoozham",
+            "Auther": "MT Vasudevan Nair",
+            "Publication": "Current Books",
+            "Category": "Novel",
+            "Shelf No": 8,
+            "Row No": 2,
+            "Published Date": "11-12-1987",
+            "Description": "The novel is a retelling of the Indian epic Mahabharata from the perspective of Bhima, the second Pandava. The story deviates from the traditional Mahabharata story as it avoids the divine elements of the ancient epic and re-represent the characters and events realistically.",
+            "purchased year": "2000",
+            "image": "image.jpg",
+        }
+    ]
+    console.log(books)
+    return books;
+
+}
