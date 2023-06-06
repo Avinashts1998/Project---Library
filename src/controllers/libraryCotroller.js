883d9a31ef2eb68619d5bceadab31b97fc81fe4a
= <=>
2023-06-06 15:31:22 +0530

    insert and get books

--- src/controllers/libraryCotroller.js
+++ src/controllers/libraryCotroller.js
@@ -1,20 +1,60 @@
 
 const LibraryFunction = require('./../utils/libraryFunction')
+const Books_model = require("./../models/booksModel")
+const dotenv = require('dotenv')
+dotenv.config({ path: "./config.env" })
+
 
 
 exports.getAllBooks = async (req, res, next) => {
+    try {
+        LibraryFns = new LibraryFunction()
 
-    LibraryFns = new LibraryFunction()
+        const argsData = (req.body.args) ? req.body.args : "";
+        const authData = (argsData.auth) ? argsData.auth : "";
 
-    const argsData = (req.body.args) ? req.body.args : "";
-    const authData = (argsData.auth) ? argsData.auth : "No data";
+        const dbName = process.env.LIBRARY_DATABASE
+        const booksColection = "Books"
 
-    const data = await LibraryFns.getAllBooks(argsData, authData)
-    // console.log(authData)
+        const data = await LibraryFns.getAllBooks(dbName, booksColection)
 
-    res.status(200).json({
-        data: data,
-        success: true,
-        result: false
-    })
+
+        res.status(200).json({
+            data: data,
+            success: true,
+            result: true
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+exports.insertBooks = async (req, res, next) => {
+    try {
+        const argsData = (req.body.args) ? req.body.args : "";
+        const authData = (argsData.auth) ? argsData.auth : "";
+
+        const paramsData = (argsData.params) ? argsData.params : "";
+
+        LibraryFns = new LibraryFunction()
+
+        const dbName = process.env.LIBRARY_DATABASE
+        const booksColection = "Books"
+
+        const data = await LibraryFns.insertBooks(dbName, booksColection, paramsData)
+
+        res.status(200).json({
+            success: true,
+            data: data,
+            message: "Books Inserted Succesfully"
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
 }
